dfe2a0bb34b58e32a4fbc5c76952f3bbbd147b4b
jmbillings <james@puffle.co.uk>
2016-05-03 20:25:01 +0100

    Add first test for empty string returning zero

---
+++ CalculatorKata.Tests/Tests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorKata.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        Calculator m_Calculator;
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            m_Calculator = new Calculator();
+        }
+
+        [Test]
+        public void EmptyStringReturnsZero()
+        {
+            Assert.AreEqual(0, m_Calculator.Add(""));
+        }
+    }
+}
